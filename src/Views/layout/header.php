6993d790449c7a251b9662b50f20da7223ee2241
Rulos21-code <jaykelalejandroperez@gmail.com>
2026-05-29 17:35:09 -0400

    Update header.php

--- src/Views/layout/header.php
+++ src/Views/layout/header.php
@@ -117,7 +117,7 @@ window.onpageshow = function(event) {
       <path
         d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
     </svg>
-
+    <h3 class="ms-3">Usuario: <span class="text-primary"><?= htmlspecialchars($_SESSION['nombre']) ?></span></h3>
     <div class="d-flex align-items-center ms-auto me-4" id="bcvDiv" style="cursor:pointer;" data-bs-toggle="modal" data-bs-target="#modalTasa">
         <img class="img-bcv me-2" src="assets/img/bcv2.png" alt="BCV" width="40">
         <span class="text-dark fw-semibold me-2">Dólar BCV:</span>
