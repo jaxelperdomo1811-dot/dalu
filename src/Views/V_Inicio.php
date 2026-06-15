c3706c1ec812ddd7e305cbd3bc4432666256186c
Rulo21-code <jaykelalejandroperez@gmail.com>
2026-06-15 09:50:02 -0400

    sql

--- src/Views/V_Inicio.php
+++ src/Views/V_Inicio.php
@@ -9,6 +9,8 @@
     <link rel="stylesheet" href="assets/bootstrap.min.css">
     <link rel="icon" href="assets/img/dalulisto.png">
     <script src="assets/js/js.js" defer></script>
+    <script src="assets/js/libs/jquery-3.7.1.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>
@@ -26,7 +28,7 @@
                     <div class="card-body">
                         <svg class="icon-bg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z"/></svg>
                         <h5 class="card-title">Total Clientes</h5>
-                        <p class="card-text"><?php echo htmlspecialchars($cantidadClientes ?? 0); ?></p>
+                        <p class="card-text text-white"><?php echo htmlspecialchars($cantidadClientes ?? 0); ?></p>
                     </div>
                     <div class="card-footer">
                         <a href="?c=clientes" class="btn-link">Ir a Clientes <span style="margin-left:5px;">&rarr;</span></a>
@@ -38,7 +40,7 @@
                     <div class="card-body">
                         <svg class="icon-bg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M21 16.5c0 .38-.21.71-.53.88l-7.9 4.44c-.16.12-.36.18-.57.18s-.41-.06-.57-.18l-7.9-4.44A.991.991 0 0 1 3 16.5v-9c0-.38.21-.71.53-.88l7.9-4.44c.16-.12.36-.18.57-.18s.41.06.57.18l7.9 4.44c.32.17.53.5.53.88v9zM12 4.15L6.04 7.5 12 10.85l5.96-3.35L12 4.15zM5 15.91l6 3.38v-6.71L5 9.19v6.72zm14 0v-6.72l-6 3.39v6.71l6-3.38z"/></svg>
                         <h5 class="card-title">Total Productos</h5>
-                        <p class="card-text"><?php echo htmlspecialchars($cantidadProductos ?? 0); ?></p>
+                        <p class="card-text text-white"><?php echo htmlspecialchars($cantidadProductos ?? 0); ?></p>
                     </div>
                     <div class="card-footer">
                         <a href="?c=productos" class="btn-link">Ir a Productos <span style="margin-left:5px;">&rarr;</span></a>
@@ -50,7 +52,7 @@
                     <div class="card-body">
                         <svg class="icon-bg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 4.5c1.38 0 2.5 1.12 2.5 2.5s-1.12 2.5-2.5 2.5-2.5-1.12-2.5-2.5 1.12-2.5 2.5-2.5zM12 18c-2.43 0-4.63-1.07-6.14-2.78.29-1.28 2.87-2.22 6.14-2.22s5.85.94 6.14 2.22C16.63 16.93 14.43 18 12 18z"/></svg>
                         <h5 class="card-title">Usuarios del Sistema</h5>
-                        <p class="card-text"><?php echo htmlspecialchars($cantidadUsuarios ?? 0); ?></p>
+                        <p class="card-text text-white"><?php echo htmlspecialchars($cantidadUsuarios ?? 0); ?></p>
                     </div>
                     <div class="card-footer">
                         <a href="?c=usuarios" class="btn-link">Ir a Usuarios <span style="margin-left:5px;">&rarr;</span></a>
--- src/Views/V_login.php
+++ src/Views/V_login.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="assets/bootstrap.min.css">
     <link rel="icon" href="assets/img/dalulisto.png">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="assets/login.css">
+    <link rel="stylesheet" href="assets/css/login.css">
 </head>
 
 <body>
